d64c07716b95369324240727af0cdc5ac7a1285a
Muhamad Gibran <49028961+M-Gibran@users.noreply.github.com>
2020-12-30 22:28:24 +0700

    auth

---
+++ src/store/actions/auth.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+import * as actionTypes from "./actionTypes";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const auth = (email, password, isSignUp) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCCN0O9jlsBL6fLpctMyYoC7wVdxmfN-xc";
+    if (!isSignUp) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCCN0O9jlsBL6fLpctMyYoC7wVdxmfN-xc";
+    }
+    axios
+      .post(url, authData)
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
